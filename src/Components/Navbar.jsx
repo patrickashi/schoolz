afe17798446c10ff1ce424d506c19d1073c75d3e
Patrick Ashi <patrickpeko1@gmail.com>
2024-11-23 11:47:06 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -52,7 +52,7 @@ const  Navbar = () => {
                 
             </div>
             <button className="py-2 flex px-2 hover:underline hover:cursor-pointer bg-[#925FE2] rounded-lg text-white" >
-                <Link to="http://127.0.0.1:8000/login/"> 
+                <Link to="https://ocohstech.onrender.com/"> 
                     <div className="flex">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-1">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
@@ -95,7 +95,7 @@ const  Navbar = () => {
                 </ul>
 
                 <button className="py-2 flex px-2 ml-4 hover:underline hover:cursor-pointer bg-[#925FE2] rounded-lg text-white" >
-                    <Link to=""> 
+                    <Link to="https://ocohstech.onrender.com/"> 
                         <div className="flex">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-1">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
